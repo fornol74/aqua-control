8deedf8b8b92afa24cf50eef78f0969f2ee98e67
fornol74 <fornol1@wp.pl>
2020-05-30 22:37:53 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install memcached
+
+pip3 install PCA9685-driver
+pip3 install w1thermsensor
+pip3 install gpiozero
+pip3 install pymemcache
+
+
+rm -rf /usr/local/bin/aqua-control
+cp -r ./aqua-control /usr/local/bin
+
+cp ./system/aqua_control.service /etc/systemd/system
+
+systemctl enable aqua_control.service

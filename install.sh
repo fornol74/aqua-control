7cb3372a85ea602b5f66e418d911e398a5fa381f
fornol74 <fornol1@wp.pl>
2020-08-06 23:34:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@ apt install python3-pip
 #Below is required by PCA9685-driver
 apt install build-essential libi2c-dev i2c-tools python-dev libffi-dev
 
+#enable i2c
+sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt
+
+#enable 1-wire
+grep -q '^dtoverlay=w1-gpio' /boot/config.txt || echo 'dtoverlay=w1-gpio' >> /boot/config.txt
+
+#enable hardware pwm
+grep -q '^dtoverlay=pwm' /boot/config.txt || echo 'dtoverlay=pwm' >> /boot/config.txt
+
 pip3 install PCA9685-driver
 pip3 install w1thermsensor
 pip3 install gpiozero
